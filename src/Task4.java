fa50db931fa442375ff169da7b4a979c64fbfcc0
Jora <jor.meliksetyan@bk.ru>
2021-12-12 11:40:55 +0400

    homework

--- src/Task4.java
+++ src/Task4.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
+        
         Random random = new Random();
         int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
