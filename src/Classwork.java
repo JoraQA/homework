dc264835781e5b363abd0f770983b2b9dbbc64f5
Jora <jor.meliksetyan@bk.ru>
2021-12-12 11:25:03 +0400

    msg

---
+++ src/Classwork.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Classwork {
+    public static int binaryToDecimal(String binary){
+        int length = binary.length();
+        int decimal = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            int numericValue = Character.getNumericValue(binary.charAt(i));
+            decimal += numericValue * Math.pow(2, length - i - 1);
+        }
+        return decimal;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String binary = scanner.nextLine();
+        System.out.println(binaryToDecimal(binary));
+    }
+
+}
+
